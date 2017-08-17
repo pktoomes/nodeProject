4e5eb5933f5179cfd37fd185f878bfc36151de80
Kelly Toomes <kelly@Kellys-MacBook.local>
2017-08-17 05:09:30 -0400

    daylight saving

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ var secDay = (((parseInt(moment().format('H')))*60*60) + ((parseInt(moment().for
 console.log(secDay);
 
 
+
 console.log('It is ' + chalk.blue(moment().format('dddd,MMMM Do, YYYY, h:mm:ss a')))
 console.log('It is the ' + chalk.magenta(moment().format('DDDo'))+' day of the year')
 console.log('It is '+ chalk.cyan(secDay)+' seconds into the day')
+if(moment().isDST() =="true"){
+  console.log('It '+ chalk.green('is')+ ' during Daylight Savings Time');
+}
+else{
+  console.log('It '+ chalk.green('is not')+ ' during Daylight Savings Time');
+}
+if(moment().isLeapYear() =="true"){
+  console.log('It '+ chalk.green('is')+ ' a Leap Year');
+}
+else{
+  console.log('It '+ chalk.green('is not')+ ' a Leap Year');
+}
